de72744241dbf24e8f5043709f870fa85bb24c64
love-adelar <love.adelar@gmail.com>
2020-10-29 17:23:17 +0900

    GET, POST에 필요한 라우터 생성

--- flask_project/app.py
+++ flask_project/app.py
@@ -1,13 +1,21 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'This is Home!'
+    return render_template('index.html')
 
-@app.route('/mypage')
-def mypage():
-    return 'This is My page!'
+@app.route('/test', methods=['GET'])
+def test_get():
+    received_title = request.args.get('given_title')
+    print(received_title)
+    return jsonify({'result': 'success', 'msg': '이 요청은 GET!'})
+
+@app.route('/test', methods=['POST'])
+def test_post():
+    received_title = request.form('given_title')
+    print(received_title)
+    return jsonify({'result':'success', 'msg': '이 요청은 POST!'})
 
 if __name__ == '__main__':
     app.run('0.0.0.0', port=5000, debug=True)
